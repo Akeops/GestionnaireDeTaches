9bb14c6c14726e12dadca6bbefba92bd5513ec91
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-06-01 22:31:39 +0200

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+#Création d'un gestionnaire de tâches : développez une application qui permet aux utilisateurs de créer, organiser et gérer
+# leurs tâches quotidiennes.
+#
+#
+# Gestionnaire de tâches : Vous pouvez créer un programme qui permet aux utilisateurs
+# de gérer leurs tâches en affichant des menus, en prenant des entrées de l'utilisateur et en affichant les tâches en cours.
