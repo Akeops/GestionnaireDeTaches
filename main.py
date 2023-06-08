4d3346fd04386b5596af975efce81d0b1131a30a
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-06-08 11:41:15 +0200

    J'ai terminé la fonction addTache()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import date
 
 # Connexion à la base de données
 conn = sqlite3.connect('baseDeDonnee')
@@ -29,7 +30,6 @@ def getUtilisateurs():
 
         for row in result:
             print(f" {row[0]},  {row[1]},  {row[2]},  {row[3]}, {row[4]}")
-
         conn.close()
     except sqlite3.Error as e:
         print('Erreur lors de la récupération des utilisateurs:', e)
@@ -37,11 +37,12 @@ def getUtilisateurs():
 
 
 class Tache:
-    def __init__ (self, idT, nomT, date_creation, idU):
+    def __init__ (self, idT, nomT, date_creation, idU, description):
         self.idT = idT
         self.nomT = nomT
         self.date_creation = date_creation
         self.idU = idU
+        self.description = description
 
 
 @staticmethod
@@ -66,18 +67,57 @@ def getTachesByIdU(idU):
         conn = sqlite3.connect('baseDeDonnee')
         cursor = conn.cursor()
 
-        # Exécution de la requête SELECT pour récupérer les données de la table spécifiée
         cursor.execute(f"SELECT * FROM tache WHERE idu = {idU}")
         result = cursor.fetchall()
         i = 1
         for row in result:
-            print(f"{i} - {row[1]},  {row[2]}, {row[4]}")
+            print(f"{i} - {row[1]}, le {row[2]} | '{row[4]}'")
             i += 1
 
+        print(f"{len(result) + 1} - Ajouter une nouvelle tâche")
+
+        choix = input('> ')
+        while not choix == len(result) + 1:
+            if (int(choix) == len(result) + 1):
+                print("Ajout d'une tâche")
+
+                print('Nom de la tâche :')
+                nomT = input('> ')
+
+                print('Description de la tâche :')
+                description = input('> ')
+                dateDuJour = date.today()
+                addTache(nomT, dateDuJour, idU, description)
+                print()
+                print('La tache a été ajouté.')
+                getTachesByIdU(idU)
+            else:
+                print("Il n'est pas possible de faire ça")
+                choix = input('> ')
+
         conn.close()
+
     except sqlite3.Error as e:
         print('Erreur lors de la récupération des utilisateurs:', e)
 
+
+
+def addTache(nomT, dateDuJour, idU, description):
+
+    try:
+        conn = sqlite3.connect('baseDeDonnee')
+        cursor = conn.cursor()
+
+        cursor.execute(f"INSERT INTO tache (nomT, date_creation, idU, description) VALUES ('{nomT}', '{dateDuJour}', {idU}, '{description}')")
+        #INSERT INTO tache(nomT, date_creation, idU, description) VALUES('nouvelleTache', '21/05/1999', 1, 'bla')
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print("Une erreur s'est produite lors de l'insertion :", e)
+
+#nomT, date_creation, idU, description
 # Section de TEST des fonctions
 '''print("\n getTaches() : \n")
 print(getTaches())
@@ -86,11 +126,15 @@ print("\n getUtilisateurs() : \n")
 print(getUtilisateurs())
 
 print("\n getUtilisateurs() : \n")
-print(getTachesByIdU(1))'''
+print(getTachesByIdU(1))
 
+print("\n addTache(nom, description, idU): : \n")
+print(addTache('Brico', 'bla', 1))'''
 
 def accueilUtilisateur(liste):
     print(f"Bienvenue {liste[2]} {liste[1]}")
+
+    print()
 
     print('Voici vos tâches :')
     getTachesByIdU(liste[0])
